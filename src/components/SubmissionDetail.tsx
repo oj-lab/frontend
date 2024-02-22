a4733aa9daf126c8ec8bda899187ef34ce1196f5
KanadeSiina <lluozheyan@gmail.com>
2024-02-22 09:29:13 +0000

    Add copy button

--- src/components/SubmissionDetail.tsx
+++ src/components/SubmissionDetail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { SubmissionServiceModel } from "../typings/submission";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import DocumentDuplicateIcon from "@heroicons/react/24/outline/DocumentDuplicateIcon";
 
 export interface SubmissionDetailProps {
   data?: SubmissionServiceModel.SubmissionInfo;
@@ -9,8 +11,7 @@ export interface SubmissionDetailProps {
 }
 
 const SubmissionDetail: React.FC<SubmissionDetailProps> = (props) => {
-  if (props && props.data)
-    console.log("lang", props.data.language.toLowerCase());
+  const [copyTip, setCopyTip] = React.useState("Copy code");
   return props.data ? (
     <div className="grid gap-2 overflow-x-auto">
       <div className="rounded border-2 border-slate-200">
@@ -34,16 +35,37 @@ const SubmissionDetail: React.FC<SubmissionDetailProps> = (props) => {
           </tbody>
         </table>
       </div>
-      <SyntaxHighlighter
-        style={oneDark}
-        language={props.data.language.toLowerCase()}
-        PreTag="div"
-        className="scrollbar-thin scrollbar-track-base-content/5 scrollbar-thumb-base-content/40 scrollbar-track-rounded-md scrollbar-thumb-rounded mockup-code"
-        showLineNumbers={true}
-        useInlineStyles={true}
-      >
-        {props.data.code}
-      </SyntaxHighlighter>
+      <div className="relative overflow-x-hidden">
+        <button
+          style={{
+            right: 0,
+          }}
+          className="tooltip tooltip-left absolute z-40 mr-2 mt-5"
+          data-tip={copyTip}
+        >
+          <CopyToClipboard
+            text={props.data.code}
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onCopy={async () => {
+              setCopyTip("Copied");
+              await new Promise((resolve) => setTimeout(resolve, 500));
+              setCopyTip(`Copy code`);
+            }}
+          >
+            <DocumentDuplicateIcon className="h-5 w-5 cursor-pointer text-white hover:text-blue-600" />
+          </CopyToClipboard>
+        </button>
+        <SyntaxHighlighter
+          style={oneDark}
+          language={props.data.language.toLowerCase()}
+          PreTag="div"
+          className="scrollbar-thin scrollbar-track-base-content/5 scrollbar-thumb-base-content/40 scrollbar-track-rounded-md scrollbar-thumb-rounded mockup-code"
+          showLineNumbers={true}
+          useInlineStyles={true}
+        >
+          {props.data.code}
+        </SyntaxHighlighter>
+      </div>
     </div>
   ) : (
     <div></div>
