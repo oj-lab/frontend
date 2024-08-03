181d08b2d5cecc6fcd82ca7220678fd5e0730d71
slhmy <slhmy@users.noreply.github.com>
2024-08-03 07:05:39 +0000

    deploy: 0bad7523c70e35eaaa5128c91ff2387a6246b983

---
+++ assets/javascript-gMaNxF1E.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-idKN0yKh.js";import"./Problem-SEoKi-P6.js";import"./index-84QNDsQz.js";import"./MarkdownRender-6rMzZ6Qp.js";import"./problem-AjGwESxE.js";import"./problem-GKVXSnAB.js";import"./judge-aiyzEdvw.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var l=t,d={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{l as conf,d as language};
