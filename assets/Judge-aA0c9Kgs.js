5ece4481d895967faafd8d7c0055777bc9dedcdb
slhmy <slhmy@users.noreply.github.com>
2024-08-24 07:10:33 +0000

    deploy: fc7ae100683d6455ebb698956d2ac062868f1459

---
+++ assets/Judge-aA0c9Kgs.js
@@ -0,0 +1,3 @@
+import{c as r,j as t}from"./index-6mPU3Bh6.js";import{b as d}from"./judge-jKsgbg2h.js";import{J as n}from"./JudgeTable-u3XE0Lig.js";import{M as a}from"./MarkdownRender-s2jwDCCu.js";function c(e,s){e=e||"text",e=e.toLowerCase();let o=`\`\`\`${e}
+${s}
+\`\`\``;return console.log(o),o}const f=()=>{const e=r().uid,{getJudge:s}=d(e),o=s();return o&&t.jsxs("div",{className:"grid gap-4 overflow-x-auto",children:[t.jsx("div",{className:"h-fit rounded border border-base-content/10 bg-base-100",children:t.jsx(n,{data:[o]})}),t.jsx(a,{content:c(o.language,o.code)})]})};export{f as default};
