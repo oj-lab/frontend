7b52bf71a2bd9442c043660763875953ade310ac
slhmy <slhmy@users.noreply.github.com>
2024-09-23 01:03:39 +0000

    deploy: 94a587dc1270ea475d55b6b8253d2a7c05e1d623

--- assets/Judge-4LWXa6M0.js
+++
@@ -1,3 +0,0 @@
-import{c as r,j as t}from"./index-KxqGJR1e.js";import{b as d}from"./judge-3WHR6DXS.js";import{J as n}from"./JudgeTable-WL3qB86X.js";import{M as a}from"./MarkdownRender-xgMkMby2.js";function c(e,s){e=e||"text",e=e.toLowerCase();let o=`\`\`\`${e}
-${s}
-\`\`\``;return console.log(o),o}const f=()=>{const e=r().uid,{getJudge:s}=d(e),o=s();return o&&t.jsxs("div",{className:"grid gap-4 overflow-x-auto",children:[t.jsx("div",{className:"h-fit rounded border border-base-content/10 bg-base-100",children:t.jsx(n,{data:[o]})}),t.jsx(a,{content:c(o.language,o.code)})]})};export{f as default};
---
+++ assets/Judge-t7PAE2lF.js
@@ -0,0 +1,3 @@
+import{c as r,j as t}from"./index-heu4BMK2.js";import{b as d}from"./judge-67o3f6aP.js";import{J as n}from"./JudgeTable-J1qD-u7y.js";import{M as a}from"./MarkdownRender-A1QCExaq.js";function c(e,s){e=e||"text",e=e.toLowerCase();let o=`\`\`\`${e}
+${s}
+\`\`\``;return console.log(o),o}const f=()=>{const e=r().uid,{getJudge:s}=d(e),o=s();return o&&t.jsxs("div",{className:"grid gap-4 overflow-x-auto",children:[t.jsx("div",{className:"h-fit rounded border border-base-content/10 bg-base-100",children:t.jsx(n,{data:[o]})}),t.jsx(a,{content:c(o.language,o.code)})]})};export{f as default};
