2c56e25f8c84c784b59784d5940534526159d423
slhmy <slhmy@users.noreply.github.com>
2024-09-28 12:03:22 +0000

    deploy: ade53a4717f0cda2f931227178e8d03a62dc3ef3

--- assets/javascript-w8gv_MuR.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript--_SratDm.js";import"./Problem-qCI9wYCv.js";import"./index-cUn6f-UY.js";import"./MarkdownRender-w9IuJWdO.js";import"./problem-dYn3JI3g.js";import"./problem-0KLa0fRT.js";import"./judge-lZIIhT4Q.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var l=t,d={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{l as conf,d as language};
